a5a42da450a336744eea1b45ba0171357562bbc9
Mqx <62719703+Mqxx@users.noreply.github.com>
2026-02-11 19:53:13 +0100

    fix: method type

--- src/json_rpc/notification_object.ts
+++ src/json_rpc/notification_object.ts
@@ -1,9 +1,6 @@
-import type { minecraft } from './schema/methods/index.ts';
-
 export interface NotificationObject {
   jsonrpc : '2.0',
-  // deno-lint-ignore ban-types
-  method : minecraft.MethodNames | (string & {}),
+  method : string,
   params? : unknown
   id? : never
 }
--- src/json_rpc/request_object.ts
+++ src/json_rpc/request_object.ts
@@ -1,10 +1,6 @@
-import type { minecraft } from './schema/methods/index.ts';
-
-
 export interface RequestObject {
   jsonrpc : '2.0',
-  // deno-lint-ignore ban-types
-  method : minecraft.MethodNames | (string & {}),
+  method : string,
   params? : unknown,
   id : string | number | null
 }
